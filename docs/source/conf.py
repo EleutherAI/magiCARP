1f9f3ed4e36568bde9cb09e89ca44c6c3f4ed8c0
shahbuland <smatiana1@gmail.com>
2022-01-28 16:48:56 -0500

    Added autodoc support

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -34,7 +34,11 @@ import sphinx_rtd_theme
 
 extensions = [
         'sphinx_rtd_theme',
-        'myst_parser'
+        'myst_parser',
+        'sphinx.ext.todo',
+        'sphinx.ext.viewcode',
+        'sphinx.ext.autodoc',
+        'sphinx.ext.autosummary'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
--- docs/source/training/dataset.rst
+++ docs/source/training/dataset.rst
@@ -1,9 +1,15 @@
 Preparing The Dataset
 =========================
 magiCARP contains multiple data pipelines which can be specified through the config file (see `configuring carp <config>`_). The base CARP architecture is trained using data structrued in the form of tuples containing some passage and a corresponding review.
+More information on what the pipelines should be used for is provided in architectures.
 
 **Data Pipelines** 
 
-* Base Data Pipeline
-* MLM Data Pipeline
-* Scarecrow Data Pipeline
+**Base Data Pipeline**
+
+.. autoclass:: carp.pytorch.data.register_datapipeline
+    :members:
+
+**MLM Data Pipeline**
+
+**Scarecrow Data Pipeline**
