a7f49f0df50f04c9038bf8d344be0a93e37a2bdb
LouisCastricato <lcastric@edu.uwaterloo.ca>
2022-02-15 12:04:02 -0500

    Working on fixing prompts

--- carp/experiments/distil_carp/generate_fuzzing_data.py
+++ carp/experiments/distil_carp/generate_fuzzing_data.py
@@ -1,9 +1,16 @@
 import csv
 import numpy as np
 import openai
+from neox_tokenizer import *
+
+bad_word_dict = \
+    {"0": -1000, "50256": -1000, "3353": -1000}
+# add all line breaks to the bad word dict
+for idx in line_break_token_ids:
+    bad_word_dict[str(idx)] = -1000
 
 key = ""
-with open("api_key.txt") as f:
+with open("../../pytorch/data/utils/api_key.txt") as f:
     key = f.read()
 
 # nice try ;)
@@ -23,8 +30,8 @@ def read_dataset_component(filepath):
     return data
 
 
-val_stories = read_dataset_component("val_stories.csv")
-val_crits = read_dataset_component("val_crits.csv")
+val_stories = read_dataset_component("../../pytorch/data/utils/val_stories.csv")
+val_crits = read_dataset_component("../../pytorch/data/utils/val_crits.csv")
 
 # Filter out stories that are below seven words long.
 stories_crits = list(list(filter(lambda x: (len(x[0].split()) > 7), zip(val_stories, val_crits))))
@@ -33,7 +40,7 @@ stories_crits = [[story for story, _ in stories_crits],
 val_stories = stories_crits[0]
 val_crits = stories_crits[1]
 
-train_stories = read_dataset_component("train_stories.csv")[0:10]
+train_stories = read_dataset_component("../../pytorch/data/utils/train_stories.csv")[0:10]
 examples_n = 5
 indices = np.random.choice(len(val_stories), examples_n, replace=False)
 print("[" + ", ".join(list(map(str, indices))) + "]")
@@ -46,17 +53,23 @@ for i in range(1, examples_n + 1, 1):
 
 for input_story in train_stories:
     story_prompt = prompt + str(examples_n + 1) + ". Story: " + input_story + "\nCriticism:"
+    # Biasing against EOT (0, 50256), and \n
     print(story_prompt)
+    import sys
+    sys.exit()
     completion = openai.Completion.create(
         engine="gpt-neo-20b",
         prompt=story_prompt,
         max_tokens=40,
         typical_p=0.5,
-        logit_bias={"50256": -100, "187": -100, "2490": -100},
+        logit_bias=bad_word_dict,
+        logprobs=30,
         stream=True)
+    print(input_story)
+    print("\n")
 
     # Print each token as it is returned
     for c in completion:
         print(c.choices[0].text, end='')
 
-    print("")
+    print("\n=============\n")
