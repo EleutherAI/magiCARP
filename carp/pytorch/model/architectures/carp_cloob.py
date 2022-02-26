Fixed gradient accumulation

carp/pytorch/model/architectures/carp_cloob.py
@@ -188,8 +188,10 @@ class CARPCloobTrainer(BaseTrainer):
         config: TrainConfig,
     ) -> Dict[str, TensorType[()]]:
         forward_output = self.model(passages, reviews, config)
+        
+        # Does gradient accumulation
+        self.zero_grad()
 
-        self.opt.zero_grad()
         # Encode passages in microbatches (with grad)
         for index, passage in enumerate(forward_output["pass_mbs"]):
             pass_tmp = forward_output["pass_encs"].copy()
@@ -217,8 +219,8 @@ class CARPCloobTrainer(BaseTrainer):
             self.scaler.unscale_(self.opt)
             torch.nn.utils.clip_grad_norm_(self.model.parameters(), config.grad_clip)
 
-        self.scaler.step(self.opt)
-        self.scaler.update()
+        self.torch_step()
+
         return {
             "Loss/Train": loss,
             "Acc/Forward": forward_output["forward_acc"],

carp/pytorch/model/architectures/carp_mlm.py
@@ -94,7 +94,7 @@ class CARPMLM(BaseModel):
             )
             for i in microbatch_inds
         ]
-        self.zero_grad(opt)
+        self.zero_grad()
 
         def accum_every_mb(inp: MLMBatchElement, passage: bool = True):
             with torch.cuda.amp.autocast():

carp/pytorch/training/__init__.py
@@ -100,16 +100,13 @@ class BaseTrainer(object):
         else:
             self.model.module.accum_step += 1
 
-    def zero_grad(self, opt: torch.optim.Optimizer):
+    def zero_grad(self):
         """
         Used to account for gradient accumulations. Accounts for deepspeed accumulation being different.
-        Args:
-            opt: Torch optimizer that we will zero grad if needed
-
         """
         if not self.use_deepspeed:
             if self.model.accum_step % self.model.config.grad_accum == 0:
-                opt.zero_grad()
+                self.opt.zero_grad()
 
     def eval_step(self, dataset):
         """