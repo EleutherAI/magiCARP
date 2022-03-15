1b8396cd5dda16682389bf6793fa6912ad962e03
shahbuland <ssmatian@uwaterloo.ca>
2022-03-15 15:04:14 -0400

    Simplified UMAP interactive visualization script

--- carp/examples/vis/umapvis.py
+++ carp/examples/vis/umapvis.py
@@ -1,126 +1,74 @@
 import torch
-import sys
 import matplotlib.pyplot as plt
-import numpy as np
-from torch.nn.functional import normalize
 
-from carp.configs import CARPConfig
-from carp.pytorch.model.architectures import *
 from carp.pytorch.data import *
 
-from carp.examples.vis.vis_util import spherical_coord, scatter_with_names
+from umap import UMAP
 
-CHUNK_SIZE = 512
-MAX_CHUNKS = 16 # Only embed this many chunks (set to -1 for all)
-N_CTX = 512
-SPHERICAL = False
+from carp.examples.vis.vis_util import scatter_with_names
 
-# Lots of stuff from before now encapsulated in this loader class
-# Load model
-print("Load Model")
-config = CARPConfig.load_yaml("configs/carp_cloob.yml")
-cloob_model = CARPCloob(config.model)
-cloob_model.load("checkpoints/CLOOB_CP/")
-cloob_model = cloob_model.cuda()
+# Look at SUBSET * len(Z) sized subset of data instead
+# (speeds up interactive visualization and umap)
+SUBSET = 0.2
 
-# Loader to forward pass and embed embeddings
-class Loader:
-    def __init__(self):
-        pipeline = BaseDataPipeline(path="carp/dataset")
-        self.passages = pipeline.passages
-        self.reviews = pipeline.reviews
-        
-        self.N = len(self.passages) # assume this is equal to len(reviews)
-        self.ind = 0
-        
-        self.preproc()
-        self.shuffle()
-    
-    def preproc(self):
-        self.passages = [passage[-N_CTX:] for passage in self.passages]
-        self.reviews = [review[-N_CTX:] for review in self.reviews]
+# Remove tailing zero rows from tensor
+# assumes all entries in T are l2 normalized
+def remove_zeros(T):
+    bound = len(T)
+    for i in range(len(T)):
+        if (T[i]**2).sum() < 0.25: # i.e. not 1
+            bound = i
+            break
+    return T[:bound]
 
-    def postproc(self, x):
-        x = normalize(x[0].float())
-        if SPHERICAL: x = spherical_coord(x)
-        x = x.cpu().detach().half()
-        return x
-        
-    def shuffle(self):
-        inds = torch.randperm(self.N)
-        self.passages = [self.passages[ind] for ind in inds]
-        self.reviews = [self.reviews[ind] for ind in inds]
-    
-    def __iter__(self):
-        return self
-    
-    def __next__(self):
-        # Get batch
-        passages = self.passages[self.ind * CHUNK_SIZE : (self.ind+1) * CHUNK_SIZE]
-        reviews = self.reviews[self.ind * CHUNK_SIZE : (self.ind+1) * CHUNK_SIZE]
-        
-        # Tokenize
-        tokenize = cloob_model.passage_encoder.call_tokenizer
-        p_tok_out = tokenize(passages)
-        r_tok_out = tokenize(reviews)
-        p_input_ids = p_tok_out["input_ids"]
-        p_masks = p_tok_out["attention_mask"]
-        r_input_ids = r_tok_out["input_ids"]
-        r_masks = r_tok_out["attention_mask"]
-        
-        p_input = BatchElement(p_input_ids, p_masks)
-        r_input = BatchElement(r_input_ids, r_masks)
-        
-        with torch.no_grad():
-            pass_encs, rev_encs = \
-                cloob_model.calculate_embeddings([p_input], [r_input])
-        
-        pass_encs = self.postproc(pass_encs)
-        rev_encs = self.postproc(rev_encs)
-        
-        self.ind += 1
-        
-        return pass_encs, rev_encs
-    
-    next = __next__
+# Load the encodings (assumes running this after extract_embeddings.py)
+print("Loading encodings...")
+P = torch.load("passage_encs.pt")
+R = torch.load("review_encs.pt")
 
-print("Load Dataset")
-loader = Loader()
+P = remove_zeros(P)
+R = remove_zeros(R)
 
-# Allocate tensors to store all passage and rev encodings
-LATENT_DIM = 2047 if SPHERICAL else 2048
-P = torch.zeros(loader.N, LATENT_DIM, dtype = torch.half)
-R = torch.zeros(loader.N, LATENT_DIM, dtype = torch.half)
+# Adjust below when running
+REDUCE_DIM = 50
+# Z = P # just look at passsages
+Z = R # just look at reviews
+# Z = torch.cat([P, R]) # look at both together in same domain
 
-from tqdm import tqdm
+# This script now assumes you have extracted embeddings already and that they are ordered as in dataset
+# For visualization of clusters
 
-# on 3090 ~45 minutes to do all
-i = 0
-print("Embedding Chunks")
-for (pass_encs, rev_encs) in tqdm(loader):
-    P[i:i + CHUNK_SIZE] = pass_encs
-    R[i:i + CHUNK_SIZE] = rev_encs
+print("Loading Dataset Strings...")
+pipeline = BaseDataPipeline(path="carp/dataset")
+P_names = pipeline.passages
+R_names = pipeline.reviews
 
-    i+=CHUNK_SIZE
-    if (i//CHUNK_SIZE) >= MAX_CHUNKS and MAX_CHUNKS != -1: break 
+P_names = P_names[:len(P)]
+R_names = R_names[:len(R)]
 
-P = P[:i]
-R = R[:i]
+# Z_names = P_names
+Z_names = R_names
+# Z_names = P_names + R_names
 
-P_names = loader.passages[:i]
-R_names = loader.reviews[:i]
+if SUBSET != 1:
+    print("Getting random subset")
+    def joint_shuffle(A, B): # A tensor, B list
+        assert len(A) == len(B)
+        inds = torch.randperm(len(A))
+        inds = inds[:int(SUBSET * len(Z))]
+        return A[inds], [B[i] for i in inds]
+    Z, Z_names = joint_shuffle(Z, Z_names)
 
-# UMAP stuff
-import umap
-reducer = umap.UMAP()
+print("Performing UMAP reduction...")
+embeddings = UMAP(
+    n_neighbors = 30,
+    min_dist = 0.0,
+    n_components = 2,
+    metric = 'cosine'
+).fit_transform(Z)
+# get 2d umap for visualization
 
-print("UMAP Reducing Dimensions of Embeddings")
-P = reducer.fit_transform(P)
-R = reducer.fit_transform(R)
-
-print("Plotting")
-x = np.concatenate((P[:,0], R[:,0]))
-y = np.concatenate((P[:,1], R[:,1]))
-names = P_names + R_names
-
-scatter_with_names(x, y, names)
+print("Plotting...")
+sc_x = embeddings[:,0]
+sc_y = embeddings[:,1]
+scatter_with_names(sc_x, sc_y, Z_names)
