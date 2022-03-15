afcb42f6b89f440b453906999dea76e9f6d7aec6
shahbuland <ssmatian@uwaterloo.ca>
2022-03-15 04:29:50 -0400

    Added cluster visualization

--- carp/examples/vis/hdbscan_cluster.py
+++ carp/examples/vis/hdbscan_cluster.py
@@ -14,6 +14,7 @@ def remove_zeros(T):
     return T[:bound]
 
 # Load the encodings (assumes running this after extract_embeddings.py)
+print("Loading encodings...")
 P = torch.load("passage_encs.pt")
 R = torch.load("review_encs.pt")
 
@@ -27,12 +28,14 @@ Z = R # just look at reviews
 # Z = torch.cat([P, R]) # look at both together in same domain
 
 # parameters used from UMAP HDBSCAN clustering tutorial
+print("Performing 1st UMAP reduction...")
 embeddings = UMAP(
     n_neighbors = 30,
     min_dist = 0.0,
     n_components = REDUCE_DIM
 ).fit_transform(Z)
 
+print("Performing HDBSCAN Clustering...")
 labels = HDBSCAN(
     min_samples = 8, # min number of labels we'll take
     min_cluster_size = 250 # min number of samples per label we'll take
@@ -42,3 +45,38 @@ labels = HDBSCAN(
 # proportion of labels that are not -1 (-1 meaning HDBSCAN gave up)
 quality = len(list(filter(lambda x: x != -1, labels))) / len(labels)
 print(quality)
+
+# Exit here if you're just checking HDBSCAN quality
+#exit()
+
+# For visualization of clusters
+from carp.pytorch.data import *
+
+print("Loading Dataset Strings...")
+pipeline = BaseDataPipeline(path="carp/dataset")
+P_names = pipeline.passages
+R_names = pipeline.reviews
+
+P_names = P_names[:len(P)]
+R_names = R_names[:len(R)]
+
+# Z_names = P_names
+Z_names = R_names
+# Z_names = P_names + R_names
+
+from carp.examples.vis.vis_util import scatter_with_names
+
+# data not assigned to a cluster will be black
+labels = ['black' if label == -1 else label for label in labels] 
+print("Performing 2nd UMAP reduction...")
+embeddings = UMAP(
+    n_neighbors = 30,
+    min_dist = 0.0,
+    n_components = 2
+).fit_transform(Z)
+# get 2d umap for visualization
+
+print("Plotting...")
+sc_x = embeddings[:,0]
+sc_y = embeddings[:,1]
+scatter_with_names(sc_x, sc_y, Z_names, labels)
