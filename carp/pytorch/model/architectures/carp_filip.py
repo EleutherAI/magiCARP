3f44f83fdf162a26659a7b7ab80a2cdca8b5c3ed
David Marx <david.marx84@gmail.com>
2022-03-16 20:25:02 -0700

    possibly working filip implementation?

--- carp/pytorch/model/architectures/carp_filip.py
+++ carp/pytorch/model/architectures/carp_filip.py
@@ -26,6 +26,7 @@ from carp.pytorch.data.utils.data_util import BatchElement, chunkBatchElement
 from carp.pytorch.model.encoders import get_encoder
 from carp.configs import TrainConfig
 
+import einops as eo
 from loguru import logger
 
 # TO DO: Make sure this behaves the same as CARP. 
@@ -63,7 +64,7 @@ class CARPSimRefactor(CARP):
         x: TensorType[-1, "latent_dim"],
         y: TensorType[-1, "latent_dim"],
         normalize=False,
-    ):
+    ) -> TensorType["batch_size", "batch_size"]:
         S_ij = self.item_pseudosimilarity__mode_i_to_mode_j(x,y,normalize)
         return S_ij * self.logit_scale.exp()
     
@@ -72,7 +73,7 @@ class CARPSimRefactor(CARP):
         y, #: TensorType[-1, "latent_dim"],
         x, #: TensorType[-1, "latent_dim"],
         normalize=False,
-        ):
+        ) -> TensorType["batch_size", "batch_size"]:
         return self.item_logits__mode_i_to_mode_j(y,x,normalize)
 
     def contrastive_loss(
@@ -87,6 +88,7 @@ class CARPSimRefactor(CARP):
         
         logits_ij = self.item_logits__mode_i_to_mode_j(x,y,normalize)
         logits_ji = self.item_logits__mode_j_to_mode_i(y,x,normalize)
+        #logger.debug(logits_ij.shape) [batch_size batch_size]
 
         loss_ij = F.cross_entropy(logits_ij, labels)
         loss_ji = F.cross_entropy(logits_ji, labels)
@@ -117,26 +119,34 @@ class CARPFilip(CARPSimRefactor):
         self,
         x: TensorType["batch_size", -1, "latent_dim"],
         y: TensorType["batch_size", -1, "latent_dim"],
-        normalize=False,
+        normalize=True,
     ):
         # To do
         #logger.debug(x.shape) # [batch_size, N, latent_dim]
         #logger.debug(y.shape) # [batch_size, M, latent_dim]
-        raise NotImplementedError
+        #raise NotImplementedError
+        if normalize:
+            x = F.normalize(x)
+            y = F.normalize(y)
+        y2 = eo.rearrange(y, 'b m d -> b d m')
+        # fancy broadcasting
+        x2 = x.unsqueeze(1)
+        z = torch.matmul(x2,y2)
+        return z
 
     def item_logits__mode_i_to_mode_j(
             self,
             x: TensorType["batch_size", -1, "latent_dim"],
             y: TensorType["batch_size", -1, "latent_dim"],
-            normalize=False,
+            normalize=True,
         ):
-        logger.debug(x.shape) # [batch_size, N, latent_dim]
-        logger.debug(y.shape)
+        #logger.debug(x.shape) # [batch_size, N, latent_dim]
+        #logger.debug(y.shape)
         S_ij = self.item_pseudosimilarity__mode_i_to_mode_j(x,y,normalize)
-        logger.debug(S_ij.shape)
+        #logger.debug(S_ij.shape)
         logits_ij = S_ij * self.logit_scale.exp()
-        logger.debug(logits_ij.shape)
-        return logits_ij.max(dim=-1)[0].mean(dim=-1)
+        #logger.debug(logits_ij.shape)
+        return logits_ij.max(dim=-1).values.mean(dim=-1)
 
 
 @register_trainer
