b08529ba30de5b36723fe4739020b5ba714e8c8a
David Marx <david.marx84@gmail.com>
2022-03-17 13:40:28 -0700

    generalized the carp refactor even more

--- carp/pytorch/model/architectures/carp_filip.py
+++ carp/pytorch/model/architectures/carp_filip.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('.')
 
-from typing import List
+from typing import List, Dict
 
 import torch
 import torch.nn.functional as F
@@ -76,31 +76,149 @@ class CARPSimRefactor(CARP):
         ) -> TensorType["batch_size", "batch_size"]:
         return self.item_logits__mode_i_to_mode_j(y,x,normalize)
 
-    def contrastive_loss(
+    def _compute_loss_or_acc(
+        self, 
+        x, #: TensorType[-1, "latent_dim"], 
+        y, #: TensorType[-1, "latent_dim"],
+        normalize=False,
+        logits_ij = None,
+        return_loss = True,
+        return_acc=True,
+    ):
+        n = x.shape[0]
+        labels = torch.arange(n, device=self.config.device)
+        
+        if logits_ij is None:
+            logits_ij = self.item_logits__mode_i_to_mode_j(x,y,normalize)
+
+        outv = {}
+        if return_loss:
+            outv['loss'] = F.cross_entropy(logits_ij, labels)
+        if return_acc:
+            outv['acc'] = (torch.argmax(logits_ij, dim=1) == labels).sum() / n
+        return outv
+
+
+    def loss_component__mode_i_to_mode_j(
+        self, 
+        x, #: TensorType[-1, "latent_dim"], 
+        y, #: TensorType[-1, "latent_dim"],
+        normalize=False,
+        logits_ij = None,
+    ) -> TensorType[(), float]:
+
+        d = self._compute_loss_or_acc(
+            x=x,
+            y=y,
+            normalize=normalize,
+            logits_ij = logits_ij,
+            return_loss = True,
+            return_acc=False,
+        )
+        return d['loss']
+
+
+    def loss_component__mode_j_to_mode_i(
+        self, 
+        x, #: TensorType[-1, "latent_dim"], 
+        y, #: TensorType[-1, "latent_dim"],
+        normalize=False,
+        logits_ji = None,
+    ) -> TensorType[(), float]:
+        return self.loss_component__mode_i_to_mode_j(y=y,x=x,normalize=normalize, logits_ij=logits_ji)
+
+
+    def contrastive_loss_terms(
         self, 
         x, #: TensorType[-1, "latent_dim"], 
         y, #: TensorType[-1, "latent_dim"],
         normalize=False,
     ) -> TensorType[(), float]:
 
-        n = x.shape[0]
-        labels = torch.arange(n, device=self.config.device)
-        
-        logits_ij = self.item_logits__mode_i_to_mode_j(x,y,normalize)
-        logits_ji = self.item_logits__mode_j_to_mode_i(y,x,normalize)
-        #logger.debug(logits_ij.shape) [batch_size batch_size]
+        loss_ij = self.loss_component__mode_i_to_mode_j(x,y,normalize)
+        loss_ji = self.loss_component__mode_j_to_mode_i(x,y,normalize)
+        return loss_ij, loss_ji
 
-        loss_ij = F.cross_entropy(logits_ij, labels)
-        loss_ji = F.cross_entropy(logits_ji, labels)
-        return (loss_ij + loss_ji) / 2
+    def loss_components(
+        self, 
+        x=None,
+        y=None,
+        normalize=False,
+        loss_ij=None,
+        loss_ji=None,
+        use_loss_transpose=True,
+    ):
+        if loss_ij is None:
+            loss_ij = self.loss_component__mode_i_to_mode_j(x,y,normalize)
+        if loss_ji is None:
+            loss_ji = loss_ij.T
+            if not use_loss_transpose:
+                loss_ji = self.loss_component__mode_j_to_mode_i(x,y,normalize)
+        return loss_ij, loss_ji
+
+    def contrastive_loss(
+        self, 
+        x=None,
+        y=None,
+        normalize=False,
+        loss_ij=None,
+        loss_ji=None,
+        use_loss_transpose=True,
+    ) -> TensorType[(), float]:
+        losses = self.loss_components(x=x,y=y, normalize=normalize, loss_ij=loss_ij, loss_ji=loss_ji, use_loss_transpose=use_loss_transpose)
+        return sum(losses) / len(losses)
+
+    def acc_component__mode_i_to_mode_j(
+        self, 
+        x, #: TensorType[-1, "latent_dim"], 
+        y, #: TensorType[-1, "latent_dim"],
+        normalize=False,
+        logits_ij = None,
+    ) -> TensorType[(), float]:
+
+        d = self._compute_loss_or_acc(
+            x=x,
+            y=y,
+            normalize=normalize,
+            logits_ij = logits_ij,
+            return_loss = False,
+            return_acc=True,
+        )
+        return d['acc']
+
+    def acc_component__mode_j_to_mode_i(
+        self, 
+        x, #: TensorType[-1, "latent_dim"], 
+        y, #: TensorType[-1, "latent_dim"],
+        normalize=False,
+        logits_ji = None,
+    ) -> TensorType[(), float]:
+        return self.acc_component__mode_i_to_mode_j(y=y,x=x,normalize=normalize, logits_ij=logits_ji)
+
+    def compute_accuracy(
+        self, 
+        x, #: TensorType[-1, "latent_dim"], 
+        y, #: TensorType[-1, "latent_dim"],
+        normalize=False,
+        logits_ij = None,
+        logits_ji = None,
+    ):
+        acc_ij = self.acc_component__mode_i_to_mode_j(x=x,y=y,normalize=normalize, logits_ij=logits_ij)
+        acc_ji = self.acc_component__mode_j_to_mode_i(x=x,y=y,normalize=normalize, logits_ji=logits_ji)
+        return (acc_ij + acc_ji) / 2
+
 
     # Is there a way we can inherit the typing from a method's output?
-    def compute_accuracy(
-        self,
-        x, #: TensorType[-1, "latent_dim"],
-        y, #: TensorType[-1, "latent_dim"],
-        normalize: bool = False,
-    ):
+    def compute_accuracy_OLD(
+        self, 
+        x=None,
+        y=None,
+        normalize=False,
+        loss_ij=None,
+        loss_ji=None,
+        use_loss_transpose=True,
+    ) -> TensorType[(), float]:
+        losses = self.loss_components(x=x,y=y, normalize=normalize, loss_ij=loss_ij, loss_ji=loss_ji, use_loss_transpose=use_loss_transpose)
         with torch.no_grad():
             n = x.shape[0]
             labels = torch.arange(n, device=self.config.device)
@@ -113,7 +231,7 @@ class CARPSimRefactor(CARP):
 
 @typechecked
 @register_architecture
-class CARPFilip(CARPSimRefactor):
+class CARPFilipOLD(CARPSimRefactor):
 
     def item_pseudosimilarity__mode_i_to_mode_j(
         self,
@@ -149,6 +267,158 @@ class CARPFilip(CARPSimRefactor):
         return logits_ij.max(dim=-1).values.mean(dim=-1)
 
 
+
+@typechecked
+@register_architecture
+class CARPFilip(CARPFilipOLD):
+
+    def forward(
+        self,
+        passages: BatchElement,
+        reviews: BatchElement,
+        config: TrainConfig,
+    ):
+        microbatch_inds = generate_indices(
+            passages.input_ids.shape[0], config.microbatch_size, shuffle=False
+        )
+        # Split tokens and masks into these microbatches
+        pass_mbs: List[BatchElement] = [
+            BatchElement(passages.input_ids[i], passages.mask[i])
+            for i in microbatch_inds
+        ]
+        rev_mbs: List[BatchElement] = [
+            BatchElement(reviews.input_ids[i], reviews.mask[i]) for i in microbatch_inds
+        ]
+
+        # Initially get all encodings without grad
+        pass_encs, rev_encs = self.calculate_embeddings(pass_mbs, rev_mbs)
+
+        # compute accuracy
+        forward_acc = self.compute_accuracy(torch.cat(pass_encs), torch.cat(rev_encs))
+
+        return {
+            "pass_mbs": pass_mbs,
+            "pass_encs": pass_encs,
+            "rev_mbs": rev_mbs,
+            "rev_encs": rev_encs,
+            "forward_acc": forward_acc,
+        }
+
 @register_trainer
 class CARPFilipTrainer(CARPTrainer):
-    pass
+    def train_deepspeed_step(
+        self,
+        passages: BatchElement,
+        reviews: BatchElement,
+        config: TrainConfig,
+    ):
+        #forward_output = self.model(passages, reviews, config)
+
+        microbatch_inds = generate_indices(
+            passages.input_ids.shape[0], config.microbatch_size, shuffle=False
+        )
+        
+        pass_mbs: List[BatchElement] = [
+            BatchElement(passages.input_ids[i], passages.mask[i])
+            for i in microbatch_inds
+        ]
+
+        with torch.no_grad():
+            rev_encs = [self.model.encode_reviews(r).hidden for r in reviews]
+
+        pass_temp = []
+        for i in microbatch_inds:
+            with torch.cuda.amp.autocast():
+                passage = pass_mbs[i]
+                pass_tmp_i = self.model.module.encode_passages(passage).hidden
+                pass_temp.append(pass_tmp_i)
+            loss = self.model.module.contrastive_loss(
+                torch.cat(pass_tmp), torch.cat(rev_encs)
+            )
+            self.deepspeed_backwards(loss)
+        rev_encs = None
+        pass_mbs = None
+
+
+        rev_mbs: List[BatchElement] = [
+            BatchElement(reviews.input_ids[i], reviews.mask[i])
+            for i in microbatch_inds
+        ]
+
+        with torch.no_grad():
+            pass_encs = [self.model.encode_passages(p).hidden for p in passages]
+
+        rev_temp = []
+        for i in microbatch_inds:
+            with torch.cuda.amp.autocast():
+                review = rev_mbs[i]
+                rev_tmp_i = self.model.module.encode_passages(review).hidden
+                rev_temp.append(rev_tmp_i)
+            loss = self.model.module.contrastive_loss(
+                torch.cat(rev_tmp), torch.cat(pass_encs)
+            )
+            self.deepspeed_backwards(loss)
+        pass_encs = None
+        rev_mbs = None
+
+        # Average the model gradients
+        self.average_gradients()
+
+        # Clipping
+        self.clip_gradients()
+
+        # Step the model
+        self.deepspeed_step()
+
+        return {
+            "Loss/Train": loss,
+            "Acc/Forward": forward_output["forward_acc"],
+        }
+
+    def train_torch_step(
+        self,
+        passages: BatchElement,
+        reviews: BatchElement,
+        config: TrainConfig,
+    ) -> Dict[str, TensorType[()]]:
+        forward_output = self.model(passages, reviews, config)
+
+        # does gradient accumulation
+        self.zero_grad()
+
+        # Encode passages in microbatches (with grad)
+        for index, passage in enumerate(forward_output["pass_mbs"]):
+            pass_tmp = forward_output["pass_encs"].copy()
+            with torch.cuda.amp.autocast():
+                pass_tmp[index] = self.model.encode_passages(passage).hidden
+                loss = self.model.contrastive_loss(
+                    torch.cat(pass_tmp), torch.cat(forward_output["rev_encs"])
+                )
+
+            self.torch_backwards(loss)
+
+        # Encode reviews in microbatches (with grad)
+        for index, review in enumerate(forward_output["rev_mbs"]):
+            rev_tmp = forward_output["rev_encs"].copy()  # no_grad
+            with torch.cuda.amp.autocast():
+                rev_tmp[index] = self.model.encode_reviews(review).hidden
+                # grad _just_ at positions in `index`
+                loss = self.model.contrastive_loss(
+                    torch.cat(forward_output["pass_encs"]), torch.cat(rev_tmp)
+                )
+
+            self.torch_backwards(loss)
+
+        # Average the model gradients
+        self.average_gradients()
+
+        # Clipping
+        self.clip_gradients()
+
+        # Step the model
+        self.torch_step()
+
+        return {
+            "Loss/Train": loss,
+            "Acc/Forward": forward_output["forward_acc"],
+        }
