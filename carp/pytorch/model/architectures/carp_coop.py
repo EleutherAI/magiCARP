5d588675ebc291b79e2f1dbad42badb34142cd7a
shahbuland <smatiana1@gmail.com>
2022-04-13 03:28:02 +0000

    pref learning update

--- carp/pytorch/model/architectures/carp_coop.py
+++ carp/pytorch/model/architectures/carp_coop.py
@@ -20,7 +20,7 @@ class PromptLayer(nn.Module):
         encoder: BaseEncoder,
         labels: List[str] = None,
         n_ctx: int = 10,
-        ctx_dim: int = 768,
+        ctx_dim: int = 1024,
     ):
         super().__init__()
         if labels is None:
---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+from setuptools import setup
+
+setup(name='carp',
+      version='0.1.0',
+      description='CARP RL',
+      author='Louis Castricato',
+      author_email='wirezapp@gmail.com',
+      url='',
+      packages= setuptools.find_packages(),
+     )
