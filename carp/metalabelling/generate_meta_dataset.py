15be8efdf56064246167dda2beb1056150989c80
shahbuland <ssmatian@uwaterloo.ca>
2022-04-16 05:25:57 -0400

    use overall mean for classifications, use cosine similarity to means for scores instead of l2 distance

--- carp/metalabelling/generate_meta_dataset.py
+++ carp/metalabelling/generate_meta_dataset.py
@@ -10,16 +10,15 @@ from carp.pytorch.data import *
 
 # check if centroids have been created
 try:
-    centroids = torch.load("metalabel_data/centroids.pt").half() # [2, k, d], pos and neg centroids
+    centroids = torch.load("metalabel_data/centroids.pt").half() # [3, k, d], overall, pos and neg centroids
 except:
     print("One or more pre-req files were not found")
     print("Ensure you run carp.metalabelling.get_centroids")
 
-if __name__ == "__main__":
-    # Load dataset
-    print("Load Dataset...")
-    pipeline = BaseDataPipeline(path="carp/dataset")
-    passages = pipeline.passages[:-1000] # dont take validation set from training
+# Load dataset
+print("Load Dataset...")
+pipeline = BaseDataPipeline(path="carp/dataset")
+passages = pipeline.passages[:-1000] # dont take validation set from training
 
 # We observed that previously, ~50% of reviews were unlabelled by HDBSCAN
 # Assuming a random sample of (passage, review) pairs, and assuming reviews used
@@ -27,7 +26,7 @@ if __name__ == "__main__":
 # ~50% of (passage, review) pairs will have a pseudo-label
 remove_unclassified = False
 
-n_samples = 10000
+n_samples = 50000
 
 # if we're gonna get 50%, take double to still get around above count
 if remove_unclassified:
@@ -37,7 +36,7 @@ if remove_unclassified:
 try:
     review_encs = torch.load("metalabel_data/meta_review_encs.pt")
     inds = torch.load("metalabel_data/meta_inds.pt")
-    
+    assert len(review_encs) == n_samples
 except:
     # make the random state different from what the review embeddings from centroid step were generated with
     enc_reviews(n_samples, force_fresh = True, ind_path = "metalabel_data/meta_inds.pt", enc_path = "metalabel_data/meta_review_encs.pt", random_state = 1)
@@ -62,6 +61,8 @@ if remove_unclassified:
 
     passages = [passages[i] for i in unclass_inds]
     review_encs = [review_encs[i] for i in unclass_inds]
+
+    print("Removed {} unclassified reviews".format(len(unclass_inds)))
 
 import pandas as pd
 
@@ -95,10 +96,12 @@ def make_csv_dataset(review_encs, centroids, mode = "pos"):
     df = pd.DataFrame()
     df["passages"] = passages
 
-    if mode == "pos":
-        means = centroids[0] # [k, d]
+    if mode == "overall":
+        means = centroids[0]
+    elif mode == "pos":
+        means = centroids[0]
     else:
-        means = centroids[1] # [k, d]
+        means = centroids[1]
 
     n_labels = len(means)
 
@@ -114,5 +117,4 @@ def make_csv_dataset(review_encs, centroids, mode = "pos"):
     torch.save(probs, "metalabel_data/dataset_centroid_dists_{}.pt".format(mode))
 
 if __name__ == "__main__":
-    make_csv_dataset(review_encs, centroids, mode = "pos")
-    make_csv_dataset(review_encs, centroids, mode = "neg")
+    make_csv_dataset(review_encs, centroids, mode = "overall")
