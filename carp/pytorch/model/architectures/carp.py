3493a070db19a756419d7dd427210ef000c7cb21
Shahbuland Matiana <44281577+shahbuland@users.noreply.github.com>
2022-05-11 18:35:06 -0400

    top_5_Acc -> top_k_acc

--- carp/pytorch/model/architectures/carp.py
+++ carp/pytorch/model/architectures/carp.py
@@ -57,7 +57,7 @@ class CARP(BaseModel):
             "rev_mbs": rev_mbs,
             "rev_encs": rev_encs,
             "forward_acc": forward_acc,
-            "top_5_Acc": top_k_acc,
+            "top_k_acc": top_k_acc,
         }
 
 
