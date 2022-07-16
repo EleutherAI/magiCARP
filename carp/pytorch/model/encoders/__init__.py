fd20a99cce647a71d6901a370bceb08151befcbf
shahbuland <ssmatian@uwaterloo.ca>
2022-07-16 03:04:23 -0400

    Modified base encoder to tokenizer with truncation

--- carp/pytorch/model/encoders/__init__.py
+++ carp/pytorch/model/encoders/__init__.py
@@ -92,6 +92,8 @@ class BaseEncoder(nn.Module):
             self.preprocess(string_batch),
             return_tensors="pt",  # Will they ever _not_ be pytorch tensors?
             padding=True,
+            truncation=True,
+            max_length=self.tokenizer.model_max_length
         )
 
     def forward(self, inputs_embeds=False, **kwargs):
